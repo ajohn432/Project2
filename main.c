326dc871fdb2ef2851ac706047498dbfec32c303
ajohn432 <ajohn432@charlotte.edu>
2024-07-24 21:30:58 -0400

    Adding basic changes to set up file input for project

--- main.c
+++ main.c
@@ -2,9 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-void main()
+void main(int argc, char **argv)
 {
 	//not really sure how to do graphs yet, we shall see
-	printf("XDDD");
+	printf("XDDD\n");
 
+	if (argc == 2)
+	{
+		printf("Testing 1 graph on both programs: (This is my test case)\n");
+		
+		graph_test(argv[1];
+	}
+	else if (argc == 5)
+	{
+		printf("Testing 4 (or more i guess) different graphs on each algorithm.\n
+		Below are the results for the class assignment:\n");
+		
+		for (int i = 0; i < argc; i++)
+		{
+
+		}
+
+	}
 }
